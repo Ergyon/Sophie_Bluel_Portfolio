3f4b531c54de6fb112e0c670ef7777a026b4f623
Ergyon <benoitdoudard56@gmail.com>
2025-05-21 03:01:58 +0200

    Initial commit

---
+++ FrontEnd/index.js
@@ -0,0 +1,76 @@
+
+// Recuperation des travaux et categories
+async function getWorks() {
+    try {
+        const reponse = await fetch("http://localhost:5678/api/works")
+        const works = await reponse.json()
+
+        const getCat = await fetch("http://localhost:5678/api/categories")
+        const categories = await getCat.json()
+
+        getFilters(categories, works)
+
+        const gallery = document.querySelector(".gallery")
+        gallery.innerHTML = ""
+
+        for (let i = 0; i < works.length; i++) {
+            const work = works[i]
+            const workArticle = document.createElement("figure")
+
+            const workImg = document.createElement("img")
+            workImg.src= work.imageUrl
+            workImg.alt = work.title
+
+            const workTitle = document.createElement("figcaption")
+            workTitle.innerText = work.title
+
+            workArticle.appendChild(workImg)
+            workArticle.appendChild(workTitle)
+            gallery.appendChild(workArticle)
+        } 
+    } catch (error) {
+            console.error("Impossible de collectionner les donnees");
+    }
+}
+
+function getFilters(categories, allWorks) {
+    const btnsContainer = document.querySelector(".btns-container")
+    btnsContainer.innerHTML = ""
+
+    const allBtns = document.createElement("button")
+    allBtns.innerText = "Tous"
+    allBtns.addEventListener("click", () => displayWorks(allWorks))
+    btnsContainer.appendChild(allBtns)
+
+    for (let category of categories) {
+        const button = document.createElement("button")
+        button.innerText = category.name
+
+        button.addEventListener("click", () => {
+            const filteredResults = allWorks.filter(work => work.categoryId === category.id)
+            displayWorks(filteredResults) 
+        })
+
+        btnsContainer.appendChild(button)
+    }
+}
+
+function displayWorks(works) {
+    const gallery = document.querySelector(".gallery")
+    gallery.innerHTML = ""
+
+    for (let work of works) {
+        const card = document.createElement("figure")
+        const cardImg = document.createElement("img")
+        cardImg.src = work.imageUrl
+        const cardTitle = document.createElement("figcaption")
+        cardTitle.innerText = work.title
+
+        card.appendChild(cardImg)
+        card.appendChild(cardTitle)
+        gallery.appendChild(card)
+    }
+}
+
+getWorks()
+
